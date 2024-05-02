3be9403eab520d99fa7e2ab5bc7eb358c1c3c841
Christian Muñoz <lalodm69@gmail.com>
2024-05-02 15:12:21 -0600

    4

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //routes
-app.use(require('../routes/index.js'));
+app.use(require('../routes/index'));
 
 app.listen(4000, () => {
   console.log('Server on port 4000');
