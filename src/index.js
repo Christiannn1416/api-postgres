5c4d8c17c3a85b1a28472f129b9325b19b538be8
Christian Muñoz <lalodm69@gmail.com>
2024-05-02 22:52:18 -0600

    9

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const {Pool}  = require('pg');
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -12,3 +12,8 @@ app.use(require('../routes/index'));
 
 app.listen(1000);
 console.log('Server on port 1000');
+
+const getUsers = async (req,res)=>{
+    const response =  await pool.query('select * from users;');
+    res.status(200).json(response.rows);
+ };
